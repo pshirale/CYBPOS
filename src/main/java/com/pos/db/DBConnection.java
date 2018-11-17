5fe82f6448f44bf30ea8485cda1b0832cf98ecf3
prashant shirale <pshirale1@gmail.com>
2018-11-17 18:33:34 +0530

    Added DB code

--- src/main/java/com/pos/db/DBConnection.java
+++ src/main/java/com/pos/db/DBConnection.java
@@ -1,5 +1,35 @@
 package com.pos.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+	
+	static 
+	{
+		try {
+			//Class.forName("com.microsoft.jdbc.sqlserver.SQLServerDriver");
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+public static Connection getDBConnection()
+{
+	String url="jdbc:sqlserver://localhost:1433; databaseName=CYBPOSDB";
+	String user="sa";
+	String password="scott";
+	Connection con=null;
+	try {
+		con=DriverManager.getConnection(url, user, password);
+	} catch (SQLException e) 
+	{
+		e.printStackTrace();
+	}
+	return con;
+}
 
 }
--- src/main/java/com/pos/main/StartApp.java
+++ src/main/java/com/pos/main/StartApp.java
@@ -1,10 +1,14 @@
 package com.pos.main;
 
+import com.pos.db.DBConnection;
 import com.pos.ui.LoginScreen;
 
 public class StartApp {
 
 	public static void main(String[] args) {
+		
+		
+		//System.out.println(DBConnection.getDBConnection());
 		LoginScreen app=new LoginScreen();
 		app.setVisible(true);
 	}
